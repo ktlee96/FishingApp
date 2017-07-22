31ddceaa14648db7106483b2e52cc8dd636c4633
David Lee <David Lee>
2017-07-22 16:34:48 -0700

    Scanner

--- Implementation.java
+++ Implementation.java
@@ -3,3 +3,16 @@
  *Fishing App implementation
  *7/22/2017
 */
+
+import java.util.Scanner;
+
+public class Implementation{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Location, Fish Species, Weather, Temperature: ");
+    while (scanner.hasNext()){
+      String data = scanner.nextLine();
+      System.out.println("You typed: " + data);
+    }
+  }
+}
