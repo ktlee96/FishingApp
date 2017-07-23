e2d88e040a69e17538e3afadce95471c2cf63a07
David Lee <David Lee>
2017-07-22 20:10:10 -0700

    Interactive HashMap

--- Implementation.java
+++ Implementation.java
@@ -11,23 +11,25 @@ public class Implementation{
   public static void main(String[] args){
     HashMap table = new HashMap<ArrayList<String>, Integer>();
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Fish Species, Weather, Temperature, Water color: ");
+    System.out.print("Fish Species, Weather, Temperature, Water color: \n");
     while (scanner.hasNext()){
       String data = scanner.nextLine();
       ArrayList<String> list = new ArrayList<String>();
-      while (data.contains(",")){
-        list.add(data.substring(0,data.indexOf(",")));
-        data = data.substring(data.indexOf(",")+2);
-      }
-      list.add(data);
-      System.out.println(table.get(list));
-      if(table.get(list) == null){
-        table.put(list,1);
+      if (!data.contains(",")){
+        System.out.println(table.toString());
       }else{
-        int i = (Integer) table.get(list);
-        table.put(list, i + 1);
+        while (data.contains(",")){
+          list.add(data.substring(0,data.indexOf(",")));
+          data = data.substring(data.indexOf(",")+2);
+        }
+        list.add(data);
+        if(table.get(list) == null){
+          table.put(list,1);
+        }else{
+          int i = (Integer) table.get(list);
+          table.put(list, i + 1);
+        }
       }
-      System.out.println(table.toString());
     }
   }
 }
