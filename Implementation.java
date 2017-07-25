123295a5c3beee17bd761696666ce14d6841da07
David Lee <David Lee>
2017-07-24 21:20:24 -0700

    New data structure

--- Implementation.java
+++ Implementation.java
@@ -9,27 +9,31 @@ import java.util.*;
 
 public class Implementation{
   public static void main(String[] args){
-    HashMap table = new HashMap<ArrayList<String>, Integer>();
+    HashMap table = new HashMap<String, HashMap<String, Integer>>();
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Fish Species, Weather, Temperature, Water color: \n");
+    System.out.print("Fish Species, Weather, Temperature, Water color, Bait: \n");
     while (scanner.hasNext()){
       String data = scanner.nextLine();
-      ArrayList<String> list = new ArrayList<String>();
-      if (!data.contains(",")){
-        System.out.println(table.toString());
+      String conditions = data.substring(0,data.lastIndexOf(","));
+      String bait = data.substring(data.lastIndexOf(',')+2);
+      if(table.get(conditions) == null){
+        HashMap<String, Integer> list = new HashMap<String, Integer>();
+        list.put(bait,1);
+        table.put(conditions, list);
+        System.out.println(list.toString());
       }else{
-        while (data.contains(",")){
-          list.add(data.substring(0,data.indexOf(",")));
-          data = data.substring(data.indexOf(",")+2);
-        }
-        list.add(data);
-        if(table.get(list) == null){
-          table.put(list,1);
+        HashMap baitTable = (HashMap) table.get(conditions);
+        System.out.println(baitTable.toString());
+        if(baitTable.get(bait) == null){
+          baitTable.put(bait,1);
+          table.put(conditions, baitTable);
         }else{
-          int i = (Integer) table.get(list);
-          table.put(list, i + 1);
+          int i = (Integer) baitTable.get(bait);
+          baitTable.put(bait, i + 1);
+          table.put(conditions, baitTable);
         }
       }
+      System.out.println(table.toString());
     }
   }
 }
